40caf88070ee2bb561013e2e5161a90e8f8818e0
Glen Dragon <gdragon@harris.com>
2016-07-05 22:41:30 -0400

    Switch to using interrupts

--- GarageMonitor.ino
+++ GarageMonitor.ino
@@ -8,6 +8,8 @@ const int pinDoorIsOpen = 5;
 const int pinOpenDoorReq = 6;
 const int pinDoorSensor = 8;
 
+const int pinToggleLed = 1;
+
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
 long previousMillis = 0;        // will store last time LED was updated
@@ -25,9 +27,11 @@ enum DoorState {
 };
 boolean setState(DoorState state);
 
-
 DoorState doorState = UNKNOWN;
 DoorState oldState = UNKNOWN;
+
+volatile bool toggle = false;
+volatile bool request = false;
 
 void setup() {
 
@@ -35,13 +39,14 @@ void setup() {
   pinMode(pinLED, OUTPUT); 
   pinMode(pinDoorPulse, OUTPUT);   
   pinMode(pinDoorIsOpen, OUTPUT); 
-  pinMode(pinOpenDoorReq, INPUT_PULLUP); 
+  pinMode(pinOpenDoorReq, INPUT); 
   pinMode(pinDoorSensor, INPUT_PULLUP);
-  
-  // Set initial high state
-  digitalWrite(pinDoorPulse, HIGH); 
 
   Serial.begin(9600);
+  
+  pinMode(pinToggleLed, OUTPUT); 
+  attachInterrupt(2, isr0, RISING);
+
 }
 
 void pulseDoor()
@@ -50,6 +55,29 @@ void pulseDoor()
   delay(100);
   digitalWrite(pinDoorPulse, HIGH);    
   delay(100);
+}
+
+void isr0()
+{
+  if (toggle == false)
+    toggle = true;
+  else
+    toggle = false; 
+
+  digitalWrite(pinToggleLed, toggle);
+  request = toggle;
+}
+
+void setRequest(boolean onoff)
+{
+  if (onoff == true)
+    request = true;
+  else
+    request = false; 
+
+  toggle = request;
+  digitalWrite(pinToggleLed, toggle);
+  //digitalWrite(pinOpenDoorReq, request);
 }
 
 boolean setState(DoorState state)
@@ -70,11 +98,13 @@ void outputDoorState()
       case DOOR_OPEN:  
       case DOOR_OPENING:  
         digitalWrite(pinDoorIsOpen, true);
+        setRequest(true);
         break;
         
       case DOOR_CLOSED:  
       case DOOR_CLOSING:  
         digitalWrite(pinDoorIsOpen, false);
+        setRequest(false);
         break;
     }
 }
@@ -108,7 +138,7 @@ void loop()
   // put your main code here, to run repeatedly: 
   ticLED();
 
-  // If the Door is closing, don't do anything until it is closed
+  // If the Door is opening/closing, don't do anything until it's finished
   if (doorState == DOOR_CLOSING)
   {
     if (digitalRead(pinDoorSensor) == LOW)
@@ -117,33 +147,43 @@ void loop()
         Serial.println("Door is closed");
     }
   }
-  else
+  else if (doorState == DOOR_OPENING)
   {
     if (digitalRead(pinDoorSensor) == HIGH)
     {
       if (setState(DOOR_OPEN) == true)
         Serial.println("Door is open");
     }
+  }
+  else
+  {
+    if (digitalRead(pinDoorSensor) == HIGH)
+    {
+      if (setState(DOOR_OPEN) == true)
+      {
+        Serial.println("Door is open");
+        outputDoorState();
+      }
+    }
     else
     {
       if (setState(DOOR_CLOSED) == true)
+      {
         Serial.println("Door is closed");
+        outputDoorState();
+      }
     }
 
-    outputDoorState();
-    delay(200);  // need this delay, so the vcrx can update its state
+    /*outputDoorState();*/
+    delay(500);
       
-    if (digitalRead(pinOpenDoorReq) == LOW)
+    if (/*digitalRead(pinOpenDoorReq) == HIGH*/ request)
     {
       if (doorState == DOOR_CLOSED)
       {   
-        if (setState(DOOR_OPENING) == true)
-        {        
+        if (setState(DOOR_OPENING) == true)        
           Serial.println("Opening door");
-          pulseDoor();
-          
-          delay(5000);  // Wait for the door to react
-        }
+        pulseDoor();
       }
     }
     else
@@ -151,17 +191,14 @@ void loop()
       if (doorState == DOOR_OPEN)
       {
         if (setState(DOOR_CLOSING) == true)
-        {
           Serial.println("Closing door");
-          pulseDoor();
-          
-          delay(1000);   // Wait for the door to react
-        }
+        pulseDoor();
       }
     }
   }
   
   outputDoorState();
+  delay(500);
 
 }
 
